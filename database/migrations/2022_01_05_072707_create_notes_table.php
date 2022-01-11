d957cb6e6f52b3f1c7e40966f299fc4f1ca34588
Gary <garyleeyq1@gmail.com>
2022-01-11 17:16:12 +0800

    Initial commit & Completed user authentication

---
+++ database/migrations/2022_01_05_072707_create_notes_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateNotesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('notes', function (Blueprint $table) {
+            $table->id();
+            $table->string("title");
+            $table->text("contents");
+            $table->foreignId("user_id")->constrained("users");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('notes', function (Blueprint $table) {
+            $table->dropForeign("user_id");
+        });
+
+        Schema::dropIfExists('notes');
+    }
+}
